4944f5db5e626e6cfad5932680d1ea9d78c15cb5
Veronika B <brenevaveronika@gmail.com>
2025-05-18 23:58:52 +0400

    Cell Component base functinoality added

---
+++ src/components/Cell.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="border border-b-purple-900 rounded-lg m-auto w-6 h-6 text-center"
+       :class="getCellClass()">
+    {{ cellFill }}
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const cellFill = ref('');
+const props = defineProps({
+  x: Number,
+  y: Number,
+  value: Number,
+  state: String
+})
+
+const getCellClass = () => {
+  if (props.state === 'flagged') {
+    cellFill.value = '🚩'
+    return 'bg-purple-900 rounded-lg'
+  }
+  if (props.state === 'question') {
+    cellFill.value = '❓'
+  }
+  if (props.state === 'hidden') {
+    return 'bg-purple-900 rounded-lg'
+  }
+  if (props.state === 'revealed') {
+    if (props.value === -1) {
+      cellFill.value = '💣'
+    }
+    else {
+      cellFill.value = props.value
+    }
+    return 'bg-pink-100 rounded-lg'
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
