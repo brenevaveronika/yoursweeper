e5f9d1d8fa33728dd601a24e82cb743ed353f78e
Veronika B <brenevaveronika@gmail.com>
2025-05-21 00:09:06 +0400

    Prettir & ESLint PLugins added + existing files formatted

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <script setup>
-import Cell from "./components/Cell.vue";
-const field = [];
-for (let i = 0; i < 9; i++) {
-  field.push([]);
-  for (let j = 0; j < 9; j++) {
-    field[i].push({
-      state: 'hidden', // 'hidden' | 'revealed' | 'flagged'
-      value: null,     // null (пусто), -1 (мина), 1-8 (цифра)
-    });
+  import Cell from './components/Cell.vue';
+  const field = [];
+  for (let i = 0; i < 9; i++) {
+    field.push([]);
+    for (let j = 0; j < 9; j++) {
+      field[i].push({
+        state: 'hidden', // 'hidden' | 'revealed' | 'flagged'
+        value: null // null (пусто), -1 (мина), 1-8 (цифра)
+      });
+    }
   }
-}
 </script>
 
 <template>
-  <main class="bg-pink-100 min-h-dvh font-mono text-purple-900 p-10 flex flex-col justify-center items-center">
-    <h1 class="text-3xl"> Hello, Yoursweeper! </h1>
+  <main
+    class="bg-pink-100 min-h-dvh font-mono text-purple-900 p-10 flex flex-col justify-center items-center"
+  >
+    <h1 class="text-3xl">Hello, Yoursweeper!</h1>
     <div class="field p-4 inline-block">
       <div v-for="(row, x) in field" :key="`row-${x}`" class="flex w-fit">
         <Cell
-            v-for="(cell, y) in row"
-            :key="`cell-${x}-${y}`"
-            :x="x"
-            :y="y"
-            :value="cell.value"
-            :state="cell.state"
+          v-for="(cell, y) in row"
+          :key="`cell-${x}-${y}`"
+          :x="x"
+          :y="y"
+          :value="cell.value"
+          :state="cell.state"
         />
       </div>
     </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,12 +1,12 @@
 <template>
   <button
-      class="w-6 h-6 md:w-8 md:h-8 border flex items-center justify-center m-0.5 rounded-md hover:bg-gray-950 duration-300 cursor-pointer"
-      :class="{
+    class="w-6 h-6 md:w-8 md:h-8 border flex items-center justify-center m-0.5 rounded-md hover:bg-gray-950 duration-300 cursor-pointer"
+    :class="{
       'bg-purple-900': state === 'hidden',
-      'bg-pink-100': state === 'revealed',
+      'bg-pink-100': state === 'revealed'
     }"
-      @click.left="handleClick"
-      @click.right.prevent="handleRightClick"
+    @click.left="handleClick"
+    @click.right.prevent="handleRightClick"
   >
     <span v-if="state === 'flagged'">🚩</span>
     <span v-else-if="state === 'revealed' && value === -1">💣</span>
@@ -15,29 +15,25 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+  interface Props {
+    x: number
+    y: number
+    value: number | null
+    state: 'hidden' | 'revealed' | 'flagged' | 'question'
+  }
 
-interface Props {
-  x: number;
-  y: number;
-  value: number | null;
-  state: 'hidden' | 'revealed' | 'flagged' | 'question';
-}
+  const props = defineProps<Props>()
 
-const props = defineProps<Props>();
+  const emit = defineEmits<{
+    (e: 'click', x: number, y: number): void
+    (e: 'right-click', x: number, y: number): void
+  }>()
 
-const emit = defineEmits<{
-  (e: 'click', x: number, y: number): void;
-  (e: 'right-click', x: number, y: number): void;
-}>();
-
-const handleClick = () => emit('click', props.x, props.y);
-const handleRightClick = (e: Event) => {
-  e.preventDefault();
-  emit('right-click', props.x, props.y);
-};
-
+  const handleClick = () => emit('click', props.x, props.y)
+  const handleRightClick = (e: Event) => {
+    e.preventDefault()
+    emit('right-click', props.x, props.y)
+  }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
