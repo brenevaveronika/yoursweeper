9e8a1c17da9f6eeaa0e6e106affdd53b20ffa4e3
Veronika B <brenevaveronika@gmail.com>
2025-05-22 20:59:51 +0400

    color scheme changed + GameHeader & IconWin added

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="w-6 h-6 md:w-8 md:h-8 border flex items-center justify-center m-0.5 rounded-md hover:bg-gray-950 duration-300 cursor-pointer"
+    class="w-6 h-6 md:w-8 md:h-8 border-pink-400 border-2 flex items-center justify-center m-0.5 rounded-sm hover:bg-pink-600 hover:border-pink-600 duration-300 cursor-pointer"
     :class="{
-      'bg-purple-900': state === 'hidden',
-      'bg-pink-100': state === 'revealed'
+      'bg-pink-300': state === 'hidden',
+      'bg-pink-500': state === 'revealed'
     }"
     @click.left="handleClick"
     @click.right.prevent="handleRightClick"
@@ -16,24 +16,24 @@
 
 <script setup lang="ts">
   interface Props {
-    x: number
-    y: number
-    value: number | null
-    state: 'hidden' | 'revealed' | 'flagged' | 'question'
+    x: number;
+    y: number;
+    value: number | null;
+    state: 'hidden' | 'revealed' | 'flagged' | 'question';
   }
 
-  const props = defineProps<Props>()
+  const props = defineProps<Props>();
 
   const emit = defineEmits<{
-    (e: 'click', x: number, y: number): void
-    (e: 'right-click', x: number, y: number): void
-  }>()
+    (e: 'click', x: number, y: number): void;
+    (e: 'right-click', x: number, y: number): void;
+  }>();
 
-  const handleClick = () => emit('click', props.x, props.y)
+  const handleClick = () => emit('click', props.x, props.y);
   const handleRightClick = (e: Event) => {
-    e.preventDefault()
-    emit('right-click', props.x, props.y)
-  }
+    e.preventDefault();
+    emit('right-click', props.x, props.y);
+  };
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/icons/IconWin.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle
+      cx="12"
+      cy="12"
+      r="9.25"
+      stroke="#9333ea"
+      stroke-width="1"
+      stroke-linecap="round"
+      fill="#d8b4fe"
+    />
+    <path
+      d="M12 16.75C9.79086 16.75 8 15.6307 8 14.25C8 13.5 8.38983 13.25 12 13.25C15.6102 13.25 16 13.5 16 14.25C16 15.6307 14.2091 16.75 12 16.75Z"
+      stroke="#9333ea"
+      stroke-width="1"
+      stroke-linecap="round"
+    />
+    <circle cx="9.25" cy="9.25" r="1" stroke="#9333ea" stroke-width="1" stroke-linejoin="round" />
+    <circle cx="14.75" cy="9.25" r="1" stroke="#9333ea" stroke-width="1" stroke-linejoin="round" />
+  </svg>
+</template>
