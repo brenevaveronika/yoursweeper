7f5b859f61bd8d4e13775a3844c74b3b939e595a
Veronika B <brenevaveronika@gmail.com>
2025-05-22 22:42:19 +0400

    main heading and link to developer added

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,21 @@
 </script>
 
 <template>
-  <main class="bg-pink-100 min-h-dvh p-10 flex flex-col justify-center items-center">
-    <div class="game border-2 border-pink-500 rounded-md p-4">
+  <main class="bg-pink-100 min-h-dvh p-10 flex flex-col justify-center items-center font-mono">
+    <h1 class="text-2xl font-bold uppercase text-pink-500 mb-4">Pickme Minesweeper</h1>
+    <div class="game border md:border-2 border-pink-500 rounded-md p-4">
       <GameHeader />
       <GameField />
     </div>
+    <p class="text-pink-500 mt-10">
+      made by
+      <a
+        class="hover:text-pink-950 transition duration-200 underline"
+        href="https://github.com/brenevaveronika"
+        target="_blank"
+        >@nikaginger</a
+      >
+    </p>
   </main>
 </template>
 
